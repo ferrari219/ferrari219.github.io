48fb6f34d8a6229ccf31e0d10da4bbaebe38fd0f
ferrari219@nate.com <ferrari219@nate.com>
2021-12-29 14:55:56 +0900

    router 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from 'routers/home';
+import Detail from 'routers/detail';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:id" element={<Detail />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
