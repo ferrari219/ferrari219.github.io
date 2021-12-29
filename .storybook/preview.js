141af61721a718530165f2e9a3a6cee263b16178
ferrari219@nate.com <ferrari219@nate.com>
2021-12-29 18:00:57 +0900

    storybook preview.js error 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,25 +3,14 @@ import theme from '../src/styles/theme';
 import { ThemeProvider } from '@emotion/react';
 
 export const decorators = [
-	(Story) => {
+	(Story) => (
 		<>
 			<ThemeProvider theme={theme}>
-				<Head>
-					<meta charset="utf-8" />
-					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-					<meta
-						name="viewport"
-						content="width=device-width, initial-scale=1"
-					/>
-					<meta name="robots" content="index,follow" />
-					<meta name="referrer" contents="always" />
-					<meta name="format-detection" content="telephone=no" />
-				</Head>
 				<Globals />
 				<Story />
 			</ThemeProvider>
-		</>;
-	},
+		</>
+	),
 ];
 
 export const parameters = {
---
+++ src/components/01_Atoms/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ label }) => {
+	return <button type="button">test</button>;
+};
+
+export default Button;
